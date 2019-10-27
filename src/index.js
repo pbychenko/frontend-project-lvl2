f50e29e250d7120883ebf16328c2bf6141ba0237
pbychenko <pavelv87@gmail.com>
2019-10-27 11:40:56 +0600

    step 2: add -f option

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import commander from 'commander';
 const runApp = () => {
   commander.version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'Output format')
+    .arguments('<firstConfig> <secondConfig>')
+    .action((firstConfig, secondConfig) => {
+      console.log(firstConfig + secondConfig);
+    })
     .parse(process.argv);
 };
 
