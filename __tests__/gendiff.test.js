6c1fbf114d50acb64bbdac4d4a4d10c2b63c4cb4
pbychenko <pavelv87@gmail.com>
2019-10-31 10:25:39 +0600

    add step 5

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,19 +1,35 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-const filepath1 = `${__dirname}/__fixtures__/before.json`;
-const filepath2 = `${__dirname}/__fixtures__/after.json`;
+const jsonFilepath1 = `${__dirname}/__fixtures__/before.json`;
+const jsonFilepath2 = `${__dirname}/__fixtures__/after.json`;
+const yamlFilepath1 = `${__dirname}/__fixtures__/before.yaml`;
+const yamlFilepath2 = `${__dirname}/__fixtures__/after.yaml`;
 
 test('Check different json files', () => {
   const expPath = `${__dirname}/__fixtures__/result1`;
 
   const expected = fs.readFileSync(`${expPath}`, 'utf8');
-  expect(genDiff(filepath1, filepath2)).toBe(expected);
+  expect(genDiff(jsonFilepath1, jsonFilepath2)).toBe(expected);
 });
 
 test('Check equal json files', () => {
   const expPath = `${__dirname}/__fixtures__/result2`;
 
   const expected = fs.readFileSync(`${expPath}`, 'utf8');
-  expect(genDiff(filepath1, filepath1)).toBe(expected);
+  expect(genDiff(jsonFilepath1, jsonFilepath1)).toBe(expected);
+});
+
+test('Check different yaml files', () => {
+  const expPath = `${__dirname}/__fixtures__/result1`;
+
+  const expected = fs.readFileSync(`${expPath}`, 'utf8');
+  expect(genDiff(yamlFilepath1, yamlFilepath2)).toBe(expected);
+});
+
+test('Check equal yaml files', () => {
+  const expPath = `${__dirname}/__fixtures__/result2`;
+
+  const expected = fs.readFileSync(`${expPath}`, 'utf8');
+  expect(genDiff(yamlFilepath1, yamlFilepath1)).toBe(expected);
 });
--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import fs from 'fs';
 import _ from 'lodash';
+import path from 'path';
+import yaml from 'js-yaml';
+
+const parse = (pathToFile1, pathToFile2) => {
+  const extname1 = path.extname(pathToFile1);
+  const extname2 = path.extname(pathToFile2);
+
+  let beforeContent = {};
+  let afterContent = {};
+
+  if (extname1 === extname2) {
+    switch (extname1) {
+      case '.json':
+        beforeContent = JSON.parse(fs.readFileSync(`${pathToFile1}`, 'utf8'));
+        afterContent = JSON.parse(fs.readFileSync(`${pathToFile2}`, 'utf8'));
+        break;
+      case '.yaml':
+        beforeContent = yaml.safeLoad(fs.readFileSync(`${pathToFile1}`, 'utf8'));
+        afterContent = yaml.safeLoad(fs.readFileSync(`${pathToFile2}`, 'utf8'));
+        break;
+      default:
+        break;
+    }
+  }
+  return { beforeContent, afterContent };
+};
 
 const genDiff = (pathToFile1, pathToFile2) => {
-  const beforeContent = JSON.parse(fs.readFileSync(`${pathToFile1}`, 'utf8'));
-  const afterContent = JSON.parse(fs.readFileSync(`${pathToFile2}`, 'utf8'));
-
+  const { beforeContent, afterContent } = parse(pathToFile1, pathToFile2);
   const result = [];
 
   Object.keys(beforeContent).forEach((key) => {
