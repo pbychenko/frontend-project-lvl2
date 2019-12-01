26777c478344290cafc866f1cd7d38b728556b8c
pbychenko <pavelv87@gmail.com>
2019-12-01 11:17:33 +0600

    fix review comments 7

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,11 +1,12 @@
 import renderSimpleDiff from './simple';
 import renderPlainDiff from './plain';
+import renderJsonDiff from './json';
 
 const render = (data, type) => {
   const renderTypes = {
     simple: renderSimpleDiff,
     plain: renderPlainDiff,
-    json: JSON.stringify,
+    json: renderJsonDiff,
   };
 
   return renderTypes[type](data);
